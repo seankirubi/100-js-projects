29c0660492e6942d310eab2ecdc62ffa97d5606c
Sean Kirubi <seannganga17@gmail.com>
2025-03-29 08:52:40 +0300

    my original code

---
+++ data/01-testimonial-data.js
@@ -0,0 +1,51 @@
+const testimonial = [
+  {profile: "../images/css\ profile\ pic-1.jpeg",
+    name:'Ethan Caldwell',
+    text: 'I couldn’t be happier with my experience! The quality exceeded my expectations, and the attention to detail was impressive. Customer service was top-notch—friendly, responsive, and truly cared about my satisfaction. I’ll definitely be coming back and recommending this to everyone I know. Well done!'
+  },
+  {profile: "../images/css\ profile\ pic-2.jpeg",
+    name:'Sophia Ramirez',
+    text: 'This was one of the best purchases I\’ve made in a long time. The product is well-made, performs exactly as advertised, and arrived even sooner than expected. It\’s rare to find something that lives up to the hype, but this absolutely did. Highly recommend\!'
+  },
+  {profile: "../images/css\ profile\ pic-3.jpeg",
+    name: 'Zuri Ndlovu',
+    text: 'I was skeptical at first, but wow—this completely blew me away! The craftsmanship is incredible, and the usability is top-tier. It’s rare to find such a perfect balance of quality and affordability. If you’re on the fence, just go for it! You won’t regret it.'
+  },
+  {profile: "../images/css\ profile\ pic-4.jpeg",
+    name: 'Kwame Okafor',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore dolores esse nostrum eum mollitia asperiores quasi possimus est architecto, numquam recusandae in obcaecati vero debitis quod aperiam quam alias vitae?'
+  }
+]
+
+function renderTestimonial(){
+  let html = [];
+  testimonial.forEach(slide => {
+    html.push(
+      `<div class="box">
+        <div class="profile-container">
+        <img class="profile" src="${slide.profile}"></img>
+        </div>
+
+        <p>${slide.text}</p>
+
+        <p class="name">${slide.name}</p>
+
+      </div>`);
+  })
+ let i = 0;
+  let intervalId = setInterval(() => {
+      if (i < html.length){
+      document.querySelector('.container').innerHTML = html[i];
+      i++;}else{
+        i = 0;
+      }
+    
+   
+  }, 5000);
+
+}
+
+console.log(renderTestimonial());
+
+
+  
