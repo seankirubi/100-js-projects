add9ee3e5529e572e606c0b1413a0626ad6ebdc0
Sean Kirubi <seannganga17@gmail.com>
2025-04-11 08:32:23 +0300

    add js

--- 3-javascript/07-rock-paper-scissors.js
+++ 3-javascript/07-rock-paper-scissors.js
@@ -1,23 +1,136 @@
-const rockEl = document.querySelector('.js-rock');
-rockEl.addEventListener('mousedown', () => {
-  rockEl.classList.add('click-rock')
-})
-rockEl.addEventListener('mouseup', () => {
-  rockEl.classList.remove('click-rock')
+
+const clickEffects = [
+  {selector: "rock", className: 'click-rock',},
+  {selector: "paper", className: 'click-paper'},
+  {selector: "scissors", className: 'click-scissors'}
+];
+
+let userChoice;
+let compChoice;
+let score = {comp:0, user:0}
+
+clickEffects.forEach(({selector, className}) => {
+  const element = document.querySelector(`.${selector}`);
+  element.addEventListener('mousedown', () => element.classList.add(className)
+  )
+  element.addEventListener('mouseup', () => {
+    setTimeout(() => {
+      element.classList.remove(className)
+    }, 500)
+    
+  })
+
+  element.addEventListener('click', () => {
+    userChoice = element.dataset[selector]
+    renderChoices()
+  })
 })
 
-const paperEl = document.querySelector('.js-paper');
-paperEl.addEventListener('mousedown', () => {
-  paperEl.classList.add('click-paper')
-})
-paperEl.addEventListener('mouseup', () => {
-  paperEl.classList.remove('click-paper')
-})
 
-const scissorsEl = document.querySelector('.js-scissors');
-scissorsEl.addEventListener('mousedown', () => {
-  scissorsEl.classList.add('click-scissors')
-})
-scissorsEl.addEventListener('mouseup', () => {
-  scissorsEl.classList.remove('click-scissors');
-})
+
+function computerChoice(){
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumber)
+
+  switch(randomNumber){
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors';
+    default:
+      return "Invalid choice";
+  }
+}
+
+function renderChoices(){
+  compChoice = computerChoice();
+  const message = compareChoices();
+  
+  let user;
+  let comp;
+  if (message === 'win'){
+    score.user++;
+    console.log(score.user)
+  }else if(message === 'lose'){
+    score.comp++;
+  }
+
+  if (userChoice === 'rock'){
+    user = '&#128074;';
+  }else if (userChoice === 'paper'){
+    user = '&#9995;';
+  }else if (userChoice === 'scissors'){
+    user = '&#9996;';
+  }
+
+  if(compChoice === 'rock'){
+    comp = '&#128074;';
+  }else if(compChoice === 'paper'){
+    comp = '&#9995;';
+  }else if(compChoice === 'scissors'){
+    comp = '&#9996;';
+  }
+
+
+  document.querySelector('.your-choice')
+    .innerHTML = `&#129308;`;
+    document.querySelector('.computer-choice')
+    .innerHTML = `&#129307;`;
+  
+  
+  setTimeout(() => {
+    document.querySelector('.your-choice')
+    .innerHTML = user;
+    document.querySelector('.computer-choice')
+    .innerHTML = comp;
+    document.querySelector('.message')
+      .innerText = `You ${message}`;
+    document.querySelector('.compScore')
+      .innerText = score.comp;
+    document.querySelector('.userScore')
+      .innerText = score.user;
+  }, 500)
+  
+}
+
+function compareChoices(){
+  console.log(userChoice, compChoice);
+  switch(userChoice){
+    case 'rock':
+      switch (compChoice) {
+        case 'rock':
+          return 'tie';
+        case 'paper':
+          return 'lose'
+        case 'scissors':
+          return 'win'
+          }
+      break;
+
+    case 'paper':
+      switch (compChoice) {
+        case 'rock':
+          return 'win';
+        case 'paper':
+          return 'tie'
+        case 'scissors':
+          return 'lose'
+          }
+      break;
+
+    case 'scissors':
+      switch (compChoice) {
+        case 'rock':
+          return 'lose';
+        case 'paper':
+          return 'win'
+        case 'scissors':
+          return 'tie'
+          }
+      break;
+  default:
+    console.log('invalid input');
+  }
+}
