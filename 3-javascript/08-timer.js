db331fb44d5d22073490dc67ad8f2f4c52e935b7
Sean Kirubi <seannganga17@gmail.com>
2025-04-13 10:27:32 +0300

    add the rest of the js for functionality

--- 3-javascript/08-timer.js
+++ 3-javascript/08-timer.js
@@ -1,7 +1,15 @@
 const startEl = document.querySelector('.start');
 const stopEl = document.querySelector('.stop');
-let isStartBtnClicked = false;
-let isStopBtnClicked = false;
+const resetEl = document.querySelector('.reset');
+const timerEl = document.querySelector('.timer');
+
+
+let intervalId = null;
+
+let centiSec = 0;
+let sec = 0;
+let min = 0;
+let hrs = 0;
 
 
 
@@ -10,6 +18,29 @@ startEl.addEventListener("click", () => {
   startEl.disabled = true;
   stopEl.disabled = false;
 
+  intervalId = setInterval(() => {
+    centiSec++
+    let centiSecString = getString(centiSec);
+    let secString = getString(sec);
+    let minString = getString(min);
+    let hrsString = getString(hrs);
+  
+    if(centiSec === 99){
+      sec++;
+      centiSec = 0;
+    }
+    if(sec === 60){
+      min++;
+      sec = 0
+    }
+    if (min === 60){
+      hrs++;
+      min = 0;
+    }
+    timerEl.textContent = `${hrsString}:${minString}:${secString}:${centiSecString}`;
+  }, 10);
+
+  
   startEl.classList.add('click');
   stopEl.classList.remove('click');
 
@@ -23,9 +54,29 @@ stopEl.addEventListener("click", () => {
   stopEl.classList.add('click');
   startEl.classList.remove('click');
 
+  clearInterval(intervalId)
   console.log(stopEl);
   console.log('stop button clicked')
 })
 
+resetEl.addEventListener('click', () => {
+  clearInterval(intervalId)
+  startEl.disabled = false;
+  stopEl.disabled = false;
+  startEl.classList.remove('click');
+  stopEl.classList.remove('click');
+
+  centiSec = 0;
+  sec = 0;
+  min = 0;
+  hrs = 0;
+  
+  timerEl.textContent = `00:00:00`;
+})
 console.log(startEl);
 console.log(stopEl);
+
+function getString(time){
+  return time.toString().padStart(2, '0');
+}
+console.log(Date.now())
